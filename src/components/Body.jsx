f0dd89b91d9714d584922a369d606847cf52629b
Imanuel Nusantara Jodi <imanuelnjodi@gmail.com>
2020-03-19 11:51:07 +0700

    .

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,6 +8,7 @@ import Graphs from './Graphs.jsx'
 function Body() {
     return (
         <div id="body">
+            <p>821081</p>
             <StudentList />
             <Graphs />
         </div>
